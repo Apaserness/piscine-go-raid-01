908533b4f679ff8d9f9cbb8bcbc7af461f5871b2
Qjawko <Qjawko@gmail.com>
2019-10-18 19:48:04 +0600

    fastfix

--- raid1a.go
+++ raid1a.go
@@ -5,20 +5,23 @@ import (
 )
 
 func Raid1a(x, y int) {
-	for i := 1; i <= y; i++ {
-		for j := 1; j <= x; j++ {
 
-			if j == 1 && i == 1 ||
-				j == 1 && i == y ||
-				j == x && i == 1 ||
-				j == x && i == y {
-				z01.PrintRune('o')
-			} else if j > 1 && j < x && i == 1 || i == y {
-				z01.PrintRune('-')
-			} else if j == 1 || j == x && i < y && i > 1 {
-				z01.PrintRune('|')
-			} else {
-				z01.PrintRune(' ')
+	for i := 1; i <= y; i++ {
+		if x > 0 {
+			for j := 1; j <= x; j++ {
+
+				if j == 1 && i == 1 ||
+					j == 1 && i == y ||
+					j == x && i == 1 ||
+					j == x && i == y {
+					z01.PrintRune('o')
+				} else if j > 1 && j < x && i == 1 || i == y {
+					z01.PrintRune('-')
+				} else if j == 1 || j == x && i < y && i > 1 {
+					z01.PrintRune('|')
+				} else {
+					z01.PrintRune(' ')
+				}
 			}
 		}
 		z01.PrintRune('\n')
