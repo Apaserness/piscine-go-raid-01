cfa8bedbbe631dd71fd5007e603b4232ec52e6a5
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-19 14:49:40 +0600

    Create raid1c.go

---
+++ raid1c.go
@@ -0,0 +1,47 @@
+package student
+
+import "github.com/01-edu/z01"
+
+func Top(x int) {
+	for i := 1; i <= x; i++ { 
+		if i == 1 || i == x {
+			z01.PrintRune('A')
+		} else {
+			z01.PrintRune('B')
+		} 
+		}
+	z01.PrintRune(10)
+}
+
+func Middle(x, y int) {
+	for i := 2; i <= y-1; i++ {
+		for j := 1; j <= x; j++ {
+			if j == 1 || j == x {
+				z01.PrintRune('B')
+			} else {
+				z01.PrintRune(' ')
+			}
+		}
+		z01.PrintRune(10)
+	}
+}
+
+func Bottom(x int) {
+	for i := 1; i <= x; i++ { 
+		if i == 1 || i == x {
+			z01.PrintRune('C')
+		} else {
+			z01.PrintRune('B')
+		} 
+		}
+	z01.PrintRune(10)
+}
+func Raid1c(x, y int) {
+	if x >= 1 && y >= 1 {
+		Top(x)
+		Middle(x,y)
+		if y != 1 {
+			Bottom(x)
+		}
+	}
+}
