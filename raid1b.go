55f5a2088c8dcaf68d45f7b5efba2529267abe09
Apaserness <54326343+Apaserness@users.noreply.github.com>
2019-10-19 11:53:42 +0600

    Create raid1b.go

---
+++ raid1b.go
@@ -0,0 +1,30 @@
+package student
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func Raid1a(x, y int) {
+	if x > 0 && y > 0 {
+		for i := 1; i <= y; i++ {
+
+			for j := 1; j <= x; j++ {
+
+				if j == 1 && i == 1 ||
+					j == 1 && i == y ||
+					j == x && i == 1 ||
+					j == x && i == y {
+					z01.PrintRune('/')
+				} else if j > 1 && j < x && i == 1 || i == y {
+					z01.PrintRune('*')
+				} else if j == 1 || j == x && i < y && i > 1 {
+					z01.PrintRune('\')
+				} else {
+					z01.PrintRune('*')
+				}
+			}
+
+			z01.PrintRune('\n')
+		}
+	}
+}
